a4d90524968cb551e63bc91485bf9580f65c134a
Eugene Miretsky <eugene.miretsky@gmail.com>
2015-07-23 15:44:56 -0400

    sdf

--- src/main/java/com/goldengate/delivery/handler/kafka/ProducerRecordWrapper.java
+++ src/main/java/com/goldengate/delivery/handler/kafka/ProducerRecordWrapper.java
@@ -3,14 +3,18 @@ package com.goldengate.delivery.handler.kafka;
 
 import com.goldengate.delivery.handler.kafka.util.EncryptedMessage;
 import com.goldengate.delivery.handler.kafka.util.Encryptor;
+
 import org.apache.kafka.common.errors.SerializationException;
 
 import java.io.ByteArrayOutputStream;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 // pseudo-typedef for ProducerRecord<K,V> (except that ProducerRecrod is final, so had to make up my own implimentation without ingeritance)
 // Some consider it an anti-pattern, but entering the type information everywhere was just too painful
 public class ProducerRecordWrapper {
+	final private static Logger logger = LoggerFactory.getLogger(ProducerRecordWrapper.class);
 	boolean encrypt = true;
 	ProducerRecord<byte[], byte[]> rec;
 	public ProducerRecordWrapper(String topic, byte[] key, byte[] val) throws SerializationException {
@@ -23,6 +27,7 @@ public class ProducerRecordWrapper {
 	      }else{
 	    	 payload = val;
 	      }
+	      logger.info("Send message: " + payload) ;
 		  rec = new ProducerRecord<byte[], byte[]> (topic, key, payload);
 		} catch (Exception e) {
 		      // avro serialization can throw AvroRuntimeException, NullPointerException,
