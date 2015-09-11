19ffafadca429c3669d2388690622f201aa5b26b
Eugene Miretsky <eugene.miretsky@gmail.com>
2015-09-11 17:04:09 -0400

    sdf

--- goldengate/src/main/java/com/goldengate/delivery/handler/kafka/util/OperationTypes.java
+++ goldengate/src/main/java/com/goldengate/delivery/handler/kafka/util/OperationTypes.java
@@ -1,12 +1,14 @@
 /*
-*
-* Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
-*
-*/
+ *
+ * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ *
+ */
 package com.goldengate.delivery.handler.kafka.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
 
 import com.goldengate.delivery.handler.kafka.operations.DeleteOperationHandler;
 import com.goldengate.delivery.handler.kafka.operations.InsertOperationHandler;
@@ -20,27 +22,40 @@ import com.goldengate.delivery.handler.kafka.operations.UpdateOperationHandler;
  * @author Vedanth K R
  *
  */
+
 public enum OperationTypes {
 
-	INSERT(InsertOperationHandler.class),
-	UPDATE_FIELDCOMP(UpdateOperationHandler.class),
-	UPDATE_FIELDCOMP_PK(PkUpdateOperationHandler.class),
-	DELETE(DeleteOperationHandler.class);
-	
+	INSERT(InsertOperationHandler.class, OperationHandler.InsertByte),
+	UPDATE_FIELDCOMP(UpdateOperationHandler.class, OperationHandler.UpdateByte), 
+	UPDATE_FIELDCOMP_PK(PkUpdateOperationHandler.class, OperationHandler.UpdatePKByte), 
+	DELETE(DeleteOperationHandler.class, OperationHandler.DeleteByte);
+
+	static byte UnknownByte = 0x0;;
+	static byte InsertByte = 0x1;
+	static byte UpdateByte = 0x2;
+	static byte DeleteByte = 0x3;
+
 	final private Logger logger = LoggerFactory.getLogger(OperationTypes.class);
-	
+
 	private OperationHandler operationHandler;
-	private OperationTypes(Class<?> clazz)
-	{
+
+	private OperationTypes(Class<?> clazz, Byte magicByte) {
 		try {
-			operationHandler = (OperationHandler) clazz.newInstance();
+			Constructor[] ctors = clazz.getDeclaredConstructors();
+			Constructor ctor = null;
+			for (int i = 0; i < ctors.length; i++) {
+			    ctor = ctors[i];
+			    if (ctor.getGenericParameterTypes().length == 1)
+				break;
+			}
+			ctor.setAccessible(true);
+			operationHandler = (OperationHandler) ctor.newInstance(magicByte);
 		} catch (Exception e) {
-			logger.error("Unable to instantiate operation handler. "+ clazz , e);
+			logger.error("Unable to instantiate operation handler. " + clazz, e);
 		}
 	}
-	
-	public OperationHandler getOperationHandler()
-	{
+
+	public OperationHandler getOperationHandler() {
 		return operationHandler;
 	}
 }
