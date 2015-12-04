53b7a2305e9bc69e8b9753f868f903634e52cd78
Eugene Miretsky <eugene.miretsky@gmail.com>
2015-12-04 14:49:46 -0500

    sdf

--- goldengate/src/main/java/com/goldengate/delivery/handler/kafka/mutationmappers/GGSQLTypeConverter.java
+++ goldengate/src/main/java/com/goldengate/delivery/handler/kafka/mutationmappers/GGSQLTypeConverter.java
@@ -166,8 +166,8 @@ public class GGSQLTypeConverter implements AbstractSQLTypeConverter<DsColumn> {
 			return new TIMESTAMP(val).timestampValue(cal);
 		} catch (Exception e){
 			DateTimeParser[] parsers = { 
-			        DateTimeFormat.forPattern("YYYY-MM-DD:HH:MM.SS.FFFFFF" ).getParser(),// TimeStamp - for some reason the format is a bit different from the GG doc and JDBC standard
-			       // DateTimeFormat.forPattern( "yyyy-MM-dd" ).getParser() 
+			        DateTimeFormat.forPattern("yyyy-MM-dd:HH:mm.ss.SSSSSS" ).getParser(),// TimeStamp - for some reason the format is a bit different from the GG doc and JDBC standard
+			        DateTimeFormat.forPattern( "yyyy-MM-dd:HH:mm.ss.SSSSSS Z" ).getParser() 
 			        		};
 			DateTimeFormatter formatter = new DateTimeFormatterBuilder().append( null, parsers ).toFormatter();
 			return new Timestamp(formatter.parseDateTime(val).toDateTime(DateTimeZone.forTimeZone(cal.getTimeZone())).getMillis());	
